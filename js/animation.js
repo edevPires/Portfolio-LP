2524b522f212ada89b8886bfa477e13410059648
edevPires <133178329+edevPires@users.noreply.github.com>
2023-09-30 07:58:31 -0300

    Welcome Section

--- js/animation.js
+++ js/animation.js
@@ -4,6 +4,15 @@ const headerItens = document.querySelectorAll('.item');
 
 const selector = document.querySelector('.selector');
 
+const selectorReposition = item => {
+    const itemWidth = item.clientWidth + 50;
+    const itemHeight = item.clientHeight  + 20;
+
+    selector.style.height = itemHeight;
+    selector.style.maxWidth = (itemWidth) + 'px';
+    
+    selector.style.left = `calc(${item.offsetLeft}px - 25px)`;
+}
 
 headerItens.forEach((item) => {
     item.addEventListener('click', () => {
@@ -18,20 +27,22 @@ headerItens.forEach((item) => {
                 item.style.color = 'black';
             }
             
-        });    
+        });
 
-        const itemWidth = item.clientWidth + 50;
-        const itemHeight = item.clientHeight  + 20;
+        selectorReposition(item);
 
-        selector.style.height = itemHeight;
-        selector.style.maxWidth = (itemWidth) + 'px';
+        window.addEventListener('resize', () => {
+            selectorReposition(item);
+        })
+    })
+
         
-        selector.style.left = `calc(${item.offsetLeft}px - 25px)`;
-    })    
 })
 
 // header Hamburguer Menu
+
 const menu = document.querySelector('.menu');
+
 const firstBar = document.querySelector('.bar:nth-child(1)');
 const secondBar = document.querySelector('.bar:nth-child(2)');
 const thirdBar = document.querySelector('.bar:nth-child(3)');
@@ -49,7 +60,6 @@ menu.addEventListener('click', () => {
         
         secondBar.style.transition = 'opacity 0.1s ease';
         secondBar.style.opacity = 0;  
-              
     
         setTimeout(() => {
             thirdBar.style.transition = 'transform 0.2s ease-in-out';
