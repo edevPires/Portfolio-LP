8397c0819f167d245606bc337b2d7a1188f9286f
edevPires <133178329+edevPires@users.noreply.github.com>
2023-10-10 18:28:35 -0300

    Commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
+const header = document.querySelector('header');
 const headerLinks = document.querySelectorAll('.header-item');
-
+const designsContainer = document.querySelector('#designs');
+const aboutContainer = document.querySelector('#about');
+const designs = document.querySelectorAll('.design');
 
 headerLinks.forEach(link => {
     link.addEventListener('click', () => {
-        alert('click');
+        if (link.classList.contains('trabalhos')) {
+            const yOffset = designsContainer.offsetTop - header.offsetHeight;
+            window.scrollTo({
+                top: yOffset,
+                behavior: "smooth"
+            });
+        }
+        if (link.classList.contains('sobre')) {
+            const yOffset = aboutContainer.offsetTop - header.offsetHeight;
+            window.scrollTo({
+                top: yOffset,
+                behavior: "smooth"
+            });
+        }
+        
     })
 });
-
-const designsContainer = document.querySelector('#designs');
-const designs = document.querySelectorAll('.design');
 
 
 function resizeImages(design)
