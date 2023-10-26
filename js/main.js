5c310f314f2e1387e63096ed7341ef0b23df2b8b
edevPires <133178329+edevPires@users.noreply.github.com>
2023-10-26 02:06:34 -0300

    Landing page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 const header = document.querySelector('header');
 const headerLinks = document.querySelectorAll('.header-item');
+
 const designsContainer = document.querySelector('#designs');
 const aboutContainer = document.querySelector('#about');
 const designs = document.querySelectorAll('.design');
 const designSection = document.querySelectorAll('.design-section');
 
+const footerLinks = document.querySelectorAll('.footer-links .links span p')
+
+
+
 window.addEventListener('scroll', () => {
     
     const scrollCoordinades = window.scrollY;
 
-    if (scrollCoordinades > 100)
+    if (scrollCoordinades > 50)
     {
         header.style.boxShadow = 'rgba(149, 157, 165, 0.2) 0px 8px 24px';
     } else
@@ -19,25 +24,44 @@ window.addEventListener('scroll', () => {
 
 })
 
+function scroll(coordinades) {
+    window.scrollTo({
+        top: coordinades,
+        behavior: "smooth"
+    });
+}
+
 headerLinks.forEach(link => {
     link.addEventListener('click', () => {
         if (link.classList.contains('trabalhos')) {
             const yOffset = designsContainer.offsetTop - header.offsetHeight - 100;
-            window.scrollTo({
-                top: yOffset,
-                behavior: "smooth"
-            });
+            scroll(yOffset);
         }
         if (link.classList.contains('sobre')) {
             const yOffset = aboutContainer.offsetTop - header.offsetHeight;
+            scroll(yOffset)
+        }
+    })
+});
+
+footerLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (link.classList.contains('home')) {
             window.scrollTo({
-                top: yOffset,
+                top: 0,
                 behavior: "smooth"
             });
         }
-        
+        if (link.classList.contains('trabalhos')) {
+            const yOffset = designsContainer.offsetTop - header.offsetHeight - 100;
+            scroll(yOffset);
+        }
+        if (link.classList.contains('sobre')) {
+            const yOffset = aboutContainer.offsetTop - header.offsetHeight;
+            scroll(yOffset)
+        }
     })
-});
+})
 
 
 function resizeImages(design)
